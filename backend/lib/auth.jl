0bf5a2410517ef117b60e9cc783a15889721e5c5
Ola Rasmus Moen <olamoen@protonmail.com>
2025-03-19 21:13:22 +0100

    adapted the very basic auth app to the very basic templating engine

--- backend/lib/auth.jl
+++ backend/lib/auth.jl
@@ -3,16 +3,21 @@ module Auth
 using HTTP, JSON3, LibPQ
 
 using ..App: STATIC_DIR
+using ..Templates: wrap
 
 export
     authenticate,
     login_page
 
-function login_page(req::HTTP.Request)
-    path = joinpath(STATIC_DIR, "auth/login.html")
-    html_content = read(path, String)
 
-    return HTTP.Response(200, Dict("Content-Type" => "text/html"), html_content)
+function login_page(req::HTTP.Request)
+    html_path = joinpath(STATIC_DIR, "auth/login.html")
+    wrap_return = wrap(html_path)
+    if wrap_return.ok
+        html_content = wrap_return.html
+        return HTTP.Response(200, Dict("Content-Type" => "text/html"), html_content)
+    end
+    return HTTP.Response(501)
 end
 
 function authenticate()
