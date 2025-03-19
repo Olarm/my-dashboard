7484a8d64386dffd19861b2b7e9892af3ef57e35
Ola Rasmus Moen <olamoen@protonmail.com>
2025-03-19 19:25:04 +0100

    remove unused function

--- backend/lib/food.jl
+++ backend/lib/food.jl
@@ -6,15 +6,6 @@ using DataFrames, Statistics
 import ..Db
 
 
-function calories_per_day()
-    df = Db.get_food()
-    total_calories = sum(df.calories ./100 .* df.amount)
-    dt = df.dateeaten[end] - df.dateeaten[1]
-    total_days = convert(Float64, dt.value) / (1000*60*60*24)
-    return total_calories / total_days
-end
-
-
 function calories_in_out()
     food = disallowmissing(Db.get_food_per_day())
     food[!, "calories in"] = food.calories
